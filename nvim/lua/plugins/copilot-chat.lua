760f977d7c0e2ebaf3d9a140b10adb48f9f561d3
cefue <cesar.fuentes@outlook.fr>
2025-04-07 11:41:22 +0200

    added avante.lua

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,55 +1,58 @@
-return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	event = "VeryLazy",
-	branch = "main",
-	cmd = "CopilotChat",
-	opts = function()
-		local user = vim.env.USER or "User"
-		user = user:sub(1, 1):upper() .. user:sub(2)
-		return {
-			auto_insert_mode = true,
-			show_help = true,
-			question_header = "  " .. user .. " ",
-			answer_header = "  Copilot ",
-			window = {
-				width = 0.4,
-			},
-			chat_autocomplete = true,
-			selection = function(source)
-				local select = require("CopilotChat.select")
-				return select.visual(source) or select.buffer(source)
-			end,
-		}
-	end,
-	keys = {
-		{ "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
-		{ "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
-		{
-			"<leader>aa",
-			function()
-				return require("CopilotChat").toggle()
-			end,
-			desc = "Toggle (CopilotChat)",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>ax",
-			function()
-				return require("CopilotChat").reset()
-			end,
-			desc = "Clear (CopilotChat)",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>aq",
-			function()
-				local input = vim.fn.input("Quick Chat: ")
-				if input ~= "" then
-					require("CopilotChat").ask(input)
-				end
-			end,
-			desc = "Quick Chat (CopilotChat)",
-			mode = { "n", "v" },
-		},
-	},
-}
+return {}
+
+-- return {
+-- 	"CopilotC-Nvim/CopilotChat.nvim",
+-- 	event = "VeryLazy",
+-- 	branch = "main",
+-- 	cmd = "CopilotChat",
+-- 	opts = function()
+-- 		local user = vim.env.USER or "User"
+-- 		user = user:sub(1, 1):upper() .. user:sub(2)
+-- 		return {
+-- 			model = "gpt-4o-2024-11-20",
+-- 			auto_insert_mode = true,
+-- 			show_help = true,
+-- 			question_header = "  " .. user .. " ",
+-- 			answer_header = "  Copilot ",
+-- 			window = {
+-- 				width = 0.4,
+-- 			},
+-- 			chat_autocomplete = true,
+-- 			selection = function(source)
+-- 				local select = require("CopilotChat.select")
+-- 				return select.visual(source) or select.buffer(source)
+-- 			end,
+-- 		}
+-- 	end,
+-- 	keys = {
+-- 		{ "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
+-- 		{ "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+-- 		{
+-- 			"<leader>aa",
+-- 			function()
+-- 				return require("CopilotChat").toggle()
+-- 			end,
+-- 			desc = "Toggle (CopilotChat)",
+-- 			mode = { "n", "v" },
+-- 		},
+-- 		{
+-- 			"<leader>ax",
+-- 			function()
+-- 				return require("CopilotChat").reset()
+-- 			end,
+-- 			desc = "Clear (CopilotChat)",
+-- 			mode = { "n", "v" },
+-- 		},
+-- 		{
+-- 			"<leader>aq",
+-- 			function()
+-- 				local input = vim.fn.input("Quick Chat: ")
+-- 				if input ~= "" then
+-- 					require("CopilotChat").ask(input)
+-- 				end
+-- 			end,
+-- 			desc = "Quick Chat (CopilotChat)",
+-- 			mode = { "n", "v" },
+-- 		},
+-- 	},
+-- }
