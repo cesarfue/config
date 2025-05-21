67b53e1d0a982140668953247d3b30761eb08b30
Cesar Fuentes <cefuente@z2r11p6.42lyon.fr>
2025-05-21 08:34:53 +0200

    [42] feat: updated harpoon on 42 branch to newer config syntax, always light colorscheme

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -42,27 +42,28 @@ return {
 
 		-- Navigate through Harpoon list sequentially
 		vim.keymap.set("n", "<C-n>", function()
-			harpoon_ui.nav_prev()
+			harpoon:list():prev()
 		end, { desc = "Previous Harpoon file" })
 
 		vim.keymap.set("n", "<C-p>", function()
-			harpoon_ui.nav_next()
+			harpoon:list():next()
 		end, { desc = "Next Harpoon file" })
 
+		-- Direct navigation to files by index
 		vim.keymap.set("n", "<leader>p1", function()
-			harpoon_ui.nav_file(1)
+			harpoon:list():select(1)
 		end, { desc = "Harpoon to file 1" })
 
 		vim.keymap.set("n", "<leader>p2", function()
-			harpoon_ui.nav_file(2)
+			harpoon:list():select(2)
 		end, { desc = "Harpoon to file 2" })
 
 		vim.keymap.set("n", "<leader>p3", function()
-			harpoon_ui.nav_file(3)
+			harpoon:list():select(3)
 		end, { desc = "Harpoon to file 3" })
 
 		vim.keymap.set("n", "<leader>p4", function()
-			harpoon_ui.nav_file(4)
+			harpoon:list():select(4)
 		end, { desc = "Harpoon to file 4" })
 	end,
 }
