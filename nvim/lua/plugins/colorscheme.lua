fb986e991f186c68c49c36298f34445b56e6064a
Cesar Fuentes <cefuente@z2r11p6.42lyon.fr>
2025-05-21 08:34:53 +0200

    [42] feat: updated harpoon on 42 branch to newer config syntax, always light colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000,
-	},
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- },
 	{
 		"sainnhe/everforest",
 		lazy = false,
@@ -15,15 +15,15 @@ return {
 		-- priority = 1000,
 		config = function()
 			local function set_colorscheme()
-				if vim.o.background == "light" then
-					vim.g.everforest_background = "soft"
-					-- vim.g.everforest_better_performance = 1
-					vim.cmd("colorscheme everforest")
-					vim.fn.system("kitty +kitten themes --reload-in=all Everforest Light Soft")
-				else
-					vim.cmd("colorscheme kanagawa")
-					vim.fn.system("kitty +kitten themes --reload-in=all Kanagawa")
-				end
+				-- if vim.o.background == "light" then
+				vim.g.everforest_background = "soft"
+				-- vim.g.everforest_better_performance = 1
+				vim.cmd("colorscheme everforest")
+				vim.fn.system("kitty +kitten themes --reload-in=all Everforest Light Soft")
+				-- else
+				-- 	vim.cmd("colorscheme kanagawa")
+				-- 	vim.fn.system("kitty +kitten themes --reload-in=all Kanagawa")
+				-- end
 			end
 			set_colorscheme()
 			vim.api.nvim_create_autocmd("OptionSet", {
