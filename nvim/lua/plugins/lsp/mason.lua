56c84c7bd96d256119743064a140c8e04acb93b1
cefue <cesar.fuentes@outlook.fr>
2025-05-28 08:38:41 +0200

    feat: fixed ts-ls

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -5,57 +5,49 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-	opts = {
-		ensure_installed = {
-			"html",
-			"cssls",
-			"tailwindcss",
-			"svelte",
-			"lua_ls",
-			"graphql",
-			"emmet_ls",
-			"prismals",
-			"pyright",
-			"clangd",
-			"dockerls",
-			"marksman",
-			"ts_ls",
-			"eslint",
-			"prettier", -- prettier formatter
-			"stylua", -- lua formatter
-			"eslint_d",
-			"rust_analyzer",
-			"jdtls",
-		},
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
 			},
-		},
-	},
+		})
 
-	-- config = function()
-	-- 	-- import mason
-	-- 	local mason = require("mason")
-	--
-	-- 	-- import mason-lspconfig
-	-- 	local mason_lspconfig = require("mason-lspconfig")
-	--
-	-- 	local mason_tool_installer = require("mason-tool-installer")
-	--
-	-- 	-- enable mason and configure icons
-	-- 	mason.setup({})
-	--
-	-- 	mason_lspconfig.setup({
-	-- 		-- list of servers for mason to install
-	-- 	})
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"clangd",
+				"dockerls",
+				"marksman",
+				"ts_ls", -- typescript
+				"eslint",
+				"rust_analyzer",
+				"jdtls",
+			},
+		})
 
-	-- 		mason_tool_installer.setup({
-	-- 			ensure_installed = {
-	-- 			},
-	-- 		})
-	-- 	end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"eslint_d",
+			},
+		})
+	end,
 }
