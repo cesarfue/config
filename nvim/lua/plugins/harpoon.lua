772a7e770d6270c284d1eea9726251e436c52113
cefue <cesar.fuentes@outlook.fr>
2025-06-10 16:10:15 +0200

    fix: harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,43 +1,43 @@
--- return {
--- 	"ThePrimeagen/harpoon",
--- 	branch = "harpoon2",
--- 	dependencies = { "nvim-lua/plenary.nvim" },
--- 	lazy = false,
--- 	config = function()
--- 		local harpoon = require("harpoon")
---
--- 		harpoon:setup()
---
--- 		vim.keymap.set("n", "<leader>a", function()
--- 			harpoon:list():add()
--- 		end, { desc = "Add file to Harpoon" })
---
--- 		vim.keymap.set("n", "<leader>.", function()
--- 			harpoon.ui:toggle_quick_menu(harpoon:list())
--- 		end, { desc = "Show Harpoon menu" })
---
--- 		vim.keymap.set("n", "<C-n>", function()
--- 			harpoon:list():prev()
--- 		end, { desc = "Previous Harpoon file" })
---
--- 		vim.keymap.set("n", "<C-p>", function()
--- 			harpoon:list():next()
--- 		end, { desc = "Next Harpoon file" })
---
--- 		vim.keymap.set("n", "<leader>p1", function()
--- 			harpoon:list():select(1)
--- 		end, { desc = "Harpoon to file 1" })
---
--- 		vim.keymap.set("n", "<leader>p2", function()
--- 			harpoon:list():select(2)
--- 		end, { desc = "Harpoon to file 2" })
---
--- 		vim.keymap.set("n", "<leader>p3", function()
--- 			harpoon:list():select(3)
--- 		end, { desc = "Harpoon to file 3" })
---
--- 		vim.keymap.set("n", "<leader>p4", function()
--- 			harpoon:list():select(4)
--- 		end, { desc = "Harpoon to file 4" })
--- 	end,
--- }
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	lazy = false,
+	config = function()
+		local harpoon = require("harpoon")
+
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Add file to Harpoon" })
+
+		vim.keymap.set("n", "<leader>.", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Show Harpoon menu" })
+
+		vim.keymap.set("n", "<C-n>", function()
+			harpoon:list():prev()
+		end, { desc = "Previous Harpoon file" })
+
+		vim.keymap.set("n", "<C-p>", function()
+			harpoon:list():next()
+		end, { desc = "Next Harpoon file" })
+
+		vim.keymap.set("n", "<leader>p1", function()
+			harpoon:list():select(1)
+		end, { desc = "Harpoon to file 1" })
+
+		vim.keymap.set("n", "<leader>p2", function()
+			harpoon:list():select(2)
+		end, { desc = "Harpoon to file 2" })
+
+		vim.keymap.set("n", "<leader>p3", function()
+			harpoon:list():select(3)
+		end, { desc = "Harpoon to file 3" })
+
+		vim.keymap.set("n", "<leader>p4", function()
+			harpoon:list():select(4)
+		end, { desc = "Harpoon to file 4" })
+	end,
+}
--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,6 +1,5 @@
 return {
 	"neovim/nvim-lspconfig",
-	-- event = { "BufReadPre", "BufNewFile" },
 	event = "VeryLazy",
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
@@ -8,13 +7,8 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
-		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
 		local mason_lspconfig = require("mason-lspconfig")
-
-		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		local keymap = vim.keymap -- for conciseness
@@ -68,24 +62,12 @@ return {
 			end,
 		})
 
-		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
-
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-		})
 	end,
 }
